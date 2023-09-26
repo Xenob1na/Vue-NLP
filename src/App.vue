b4aa86c8c0da6e3deb3927247a6ce7422941c36d
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-09-26 20:29:54 +0700

    init

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import Header from './components/Layout/Header.vue';
+</script>
+
+<template>
+    <Header />
+</template>
+
---
+++ src/components/Layout/Header.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="max-w-[1200px] mx-auto flex justify-between items-center mt-4">
+        <div>
+            <img src="/img/logo.svg" alt="logo">
+        </div>
+        <nav>
+            <ul class="flex gap-7">
+                <li class="text-[20px] text-white font-bold font-oswald">Главная</li>
+                <li class="text-[20px] text-white font-bold font-oswald">Преимущества</li>
+                <li class="text-[20px] text-white font-bold font-oswald">Программа</li>
+                <li class="text-[20px] text-white font-bold font-oswald">Контакты</li>
+            </ul>
+        </nav>
+        <div>
+            <button class="bg-[#FFF] text-[20px] text-[#0650E6] px-[39px] py-[11px] font-bold font-oswald">Записаться</button>
+        </div>
+    </header>
+    <hr class="max-w-[1200px] mx-auto mt-5">
+</template>
+
+<script setup>
+
+</script>
