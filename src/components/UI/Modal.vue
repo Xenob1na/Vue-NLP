e132658821df2f3f0b89c88269075ab25e8c15aa
Gromov Danil <105980448+Xenob1na@users.noreply.github.com>
2023-09-30 19:59:36 +0700

    Commits3

--- src/components/UI/Modal.vue
+++ src/components/UI/Modal.vue
@@ -3,29 +3,54 @@
         <div class="modal fixed z-[1] left-0 top-0 w-[100%] h-[100%] pt-[50px] overflow-auto">
             <div class="bg-[#fefefe]  rounded-[5px] w-[30%] mx-auto lgMax:w-[50%] lg2:w-[60%] lgMax1:w-[40%]">
                 <div class="container">
-                    <span class="close text-[#aaa] float-right text-[28px] mb-[30px] font-semibold"  @click="$emit('close')">&times;</span>
-                    <form class="model-content">
+                    <span class="close text-[#aaa] float-right text-[28px] mb-[30px] font-semibold"
+                        @click="$emit('close')">&times;</span>
+                    <form class="model-content" ref="input" @submit.prevent="sendEmail">
                         <input type="text"
                             class="outline-none    font-normal  leading-4 text-[#212529] font-lato bg-clip-padding border-solid  rounded-[8px]"
-                            placeholder="Your name">
+                            placeholder="Ваше Имя" name="to_name" required v-model="name">
                         <input type="email"
                             class="outline-none   font-normal  leading-4 text-[#212529] font-lato bg-clip-padding border-solid   rounded-[8px]"
-                            placeholder="rossmartin@gmail.com">
-                        <input type="text"
-                            class="outline-none    font-normal  leading-4 text-[#212529] font-lato bg-clip-padding border-solid  rounded-[8px]"
-                            placeholder="Your phone">
+                            placeholder="Ваша почта" name="email_id" required v-model="email">
                         <button
                             class="text-[#fff] bg-[#0650E6] w-[100%] px-10 py-4 font-bold font-oswald rounded-[5px] hover:bg-[#1452B0] text-[18px] text-center">Отправить</button>
                     </form>
+                    <div v-if="error"
+                        class="text-center bg-[#0650E6] px-10 py-4 rounded-[5px] max-w-[420px] mx-auto opacity-[80%]">
+                        <p class="text-white font-lato font-medium">{{ error }}</p>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </template>
-  
-  
+   
+
 <script setup>
+import { ref } from 'vue';
+import emailjs from '@emailjs/browser';
+
 document.body.classList.toggle('no-scroll')
+
+const email = ref('')
+const name = ref('')
+const input = ref(null)
+const error = ref('')
+function sendEmail() {
+    emailjs.sendForm('service_lxmkjcn', 'template_smycxmb', input.value, 'lmMuXex_BZd7r06sn')
+        .then((result) => {
+            console.log('SUCCESS!', result.text);
+            error.value = 'Письмо отправлено успешно. Cкоро я свяжусь с вами.'
+        }, (error) => {
+            console.log('FAILED...', error.text);
+            error.value = 'Ошибка отправки'
+        });
+
+    email.value = ''
+    name.value = ''
+    error.value = ''
+}
+
 
 </script>
   
